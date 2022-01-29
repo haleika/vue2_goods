140a0f64cae2c9bfd24478a3aee94da698ea8e21
“hyb” <3164249018@qq.com>
2022-01-29 23:56:45 +0800

    项目初始化

---
+++ Study.md
@@ -0,0 +1,12 @@
+#创建
+vue create goods_project
+
+#目录
+node_modules 依赖
+public 放静态文件
+src 源代码
+    assets 放置静态资源（一般是多个组件共用的静态资源）
+
+    components 放置非路由的静态组件
+
+APP 根组件
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header />
+    <!-- 路由组件展示 -->
+    <router-view></router-view>
+    <div>{{$route.path}}</div>
+    <!-- <Footer v-show="['/home','/search'].includes($route.path)"/> -->
+    <Footer v-show="$route.meta && $route.meta.show"/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+export default {
+  name: 'App',
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
