fb2865a3a14093ee1e31892ffb9d5f540b38ef43
HeyinB <3164249018@qq.com>
2022-02-04 16:53:12 +0800

    axios封装

--- src/api/request.js
+++ src/api/request.js
@@ -2,8 +2,24 @@ import axios from 'axios'
 
 //1.利用axios对象方法create，去创建一个axios对象
 
-const request = create
+const requests = axios.create({
+    baseURL:"/api",
+    timeout:5000,
+})
+
+// 请求拦截器，在发请求前可以检测，可以在发请求前拦截一些事情
+requests.interceptors.request.use(condig=>{
+    return condig;
+})
 
 
+// 响应拦截器
+requests.interceptors.response.use(res=>{
+    return res.data;
+},error=>{
+    //响应失败的回调
+    return Promise.reject(new Error(error))
+})
 
-export default axios
+
+export default requests
