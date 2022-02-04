2976162080d4d9e733f7d1c6d4bf9a0c77a38988
HeyinB <3164249018@qq.com>
2022-02-04 17:12:52 +0800

    添加进度条

--- src/api/request.js
+++ src/api/request.js
@@ -1,4 +1,8 @@
 import axios from 'axios'
+//进度条
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+//start:进度条开始  done：进度条结束
 
 //1.利用axios对象方法create，去创建一个axios对象
 
@@ -9,15 +13,19 @@ const requests = axios.create({
 
 // 请求拦截器，在发请求前可以检测，可以在发请求前拦截一些事情
 requests.interceptors.request.use(condig=>{
+    nprogress.start()
     return condig;
 })
 
 
 // 响应拦截器
 requests.interceptors.response.use(res=>{
+    nprogress.done()
     return res.data;
 },error=>{
     //响应失败的回调
+    nprogress.done()
+
     return Promise.reject(new Error(error))
 })
 
